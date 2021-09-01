002021c7b2feb64bbd82657a080dd2879c5690f1
Kenton Troy <kentontroy@yahoo.com>
2021-09-01 13:32:45 -0400

    Create hdfsFileTest.java

---
+++ src/hdfsFileTest.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.security.PrivilegedExceptionAction;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.security.*;
+
+class FileCount {
+  public static void main(final String[] args) throws IOException, FileNotFoundException, InterruptedException {
+    String principal = "joe_analyst/cdp.cloudera.com@CLOUDERA.COM";
+    String keytab = "/etc/security/keytabs/joe_analyst.keytab";
+
+    Configuration conf = new Configuration();
+    conf.set("fs.defaultFS", "hdfs://cdp.cloudera.com:8020");
+    conf.set("hadoop.security.authentication", "kerberos");
+    conf.set("hadoop.security.authorization", "true");
+
+    UserGroupInformation.setConfiguration(conf);
+    UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(principal, keytab);
+
+    ugi.doAs(new PrivilegedExceptionAction() {
+      public Void run() throws Exception {
+        FileSystem fs = FileSystem.get(conf);
+        String dirName = "/";
+        FileStatus[] status = fs.listStatus(new Path(dirName));
+        System.out.println("File Count: " + status.length);
+
+        return null;
+      }
+   });
+  }
+}
