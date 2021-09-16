70552f5e890a5bf045f020363a1340bd88e6e249
Kenton Troy <kentontroy@yahoo.com>
2021-09-16 17:57:04 -0400

    Update HdfsFileTest.java

--- src/HdfsFileTest.java
+++ src/HdfsFileTest.java
@@ -9,31 +9,32 @@ import org.apache.hadoop.security.token.TokenIdentifier;
 public class HdfsFileTest {
   public static void main(final String[] args) throws Exception {
     Configuration conf = new Configuration();
-    conf.set("fs.defaultFS", "hdfs://cdp.cloudera.com:8020");
+    conf.set("fs.defaultFS", "hdfs://dturnau-802475-2.gce.cloudera.com:8022");
     conf.set("hadoop.security.authentication", "kerberos");
     conf.set("hadoop.security.authorization", "true");
 
     UserGroupInformation.setConfiguration(conf);
 
-    String keytab = "/etc/security/keytabs/merged_trusted_apps_1_2.keytab";
+    String keytab1 = "/etc/security/keytabs/trusted_app_1.keytab";
 
-    UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI( "trusted_app_1/cdp.cloudera.com@CLOUDERA.COM", keytab);
+    UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI("trusted_app_1@ADV.SEC.CLOUDERA.COM", keytab1);
     ugi.doAs(new PrivilegedExceptionAction() {
       public Void run() throws Exception {
         FileSystem fs = FileSystem.get(conf);
-        fs.create(new Path("/kms_test_zone_1/test3"));
+        fs.create(new Path("/kms_test_zone_1/testFile"));
         return null;
       }
     });
 
-    UserGroupInformation ugi2 = UserGroupInformation.getUGIFromTicketCache("/tmp/cache-trusted_app_2", "trusted_app_2/cdp.cloudera.com@CLOUDERA.COM");
+    String keytab2 = "/etc/security/keytabs/trusted_app_2.keytab";
+
+    UserGroupInformation ugi2 = UserGroupInformation.loginUserFromKeytabAndReturnUGI("trusted_app_2@ADV.SEC.CLOUDERA.COM", keytab2);
     ugi2.doAs(new PrivilegedExceptionAction() {
       public Void run() throws Exception {
         FileSystem fs = FileSystem.get(conf);
-        fs.create(new Path("/kms_test_zone_2/test3"));
+        fs.create(new Path("/kms_test_zone_2/testFile"));
         return null;
       }
     });
   }
 }
-~             
